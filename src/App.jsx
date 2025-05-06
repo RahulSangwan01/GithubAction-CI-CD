0a6981cb75c6a4be962629a15d78e0f01625ae42
Rahul <rahulsangwan239@gmail.com>
2025-05-06 16:40:33 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Hello World React on Docker with CI/CD</h1>
+        <p>Deploy by Rahul Sangwan</p>
+      </div>
+    </>
+  )
+}
+export default App
